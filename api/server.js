fa4c0b42470ccd1fdafd9e740680b384764b1c71
madruga665 <madruga665@hotmail.com>
2026-03-10 20:05:45 -0300

    fix: fake api

--- api/server.js
+++ api/server.js
@@ -2,10 +2,11 @@
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, '../db.json'));
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+// Adicione isso para que o Vercel entenda o roteamento
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
