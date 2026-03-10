16d0dd607b82bae8dcb51f088f503dacdbe5cd88
madruga665 <madruga665@hotmail.com>
2026-03-09 21:07:56 -0300

    feat: save username

--- src/components/signup-form/signup-form.tsx
+++ src/components/signup-form/signup-form.tsx
@@ -1,15 +1,44 @@
+'use client'
+
+import { useState } from "react";
 import { Button } from "../button/button";
 import { InputField } from "../input-field/input-field";
 import { Heading1 } from "../typography/heading/heading1";
 
 export function SignupForm() {
+  const [username, setUsername] = useState("");
+
+  const handleSubmit = (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    if (username.trim()) {
+      sessionStorage.setItem('@codeleap:username', username);
+      setUsername(""); // Limpa o input após o envio
+    }
+  };
+
   return (
-    <form className="w-125 bg-white border border-[#CCCCCC] p-6 rounded-2xl">
+    <section className="w-125 bg-white border border-[#CCCCCC] p-6 rounded-2xl">
       <Heading1 className="mb-6">Welcome to CodeLeap network!</Heading1>
-      <InputField id="username" label="Please enter your username" placeholder="John doe" />
-      <div className="flex flex-row justify-end w-full mt-4">
-        <Button variant="primary">ENTER</Button>
-      </div>
-    </form>
+
+      <form onSubmit={handleSubmit}>
+        <InputField
+          id="username"
+          label="Please enter your username"
+          placeholder="John doe"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <div className="flex flex-row justify-end w-full mt-4">
+          <Button
+            variant="primary"
+            type="submit"
+            disabled={!username.trim()}
+          >
+            ENTER
+          </Button>
+        </div>
+      </form>
+    </section>
   )
 }
